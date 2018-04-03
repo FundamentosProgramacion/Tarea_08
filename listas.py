6f93b5563894cb90c233f04865fee8cc0c1e9c45
FernandoSSilvaM <porta.hhh@gmail.com>
2018-04-03 14:56:22 -0500

    Ejercicios 2 y 4completados

--- listas.py
+++ listas.py
@@ -1,8 +1,8 @@
 # FERNANDO SEBASTAIAN SILVA MIRAMONTES
-
+# EJERCICIOS CON LISTAS.
 
 # EJERCICIO 1
-def sumarAcomulado(lista):
+def sumarAcumulado(lista):
     if lista == [] :
         return []
 
@@ -36,27 +36,57 @@ def estanOrdenados(lista):
 
     return True
 
+
 #EJERCICIO 4
 def sonAnagramas(a,b):
+    a = a.upper()
+    b = b.upper()
+    listaA = list(a)
+    listaB = list(b)
     prueba = True
+    if len(listaB) != len(listaA):
+        return False
     for dato in range(0,len(b)):
-        prueba = a[dato] in b
-        if prueba == False:
-            return False
-    return True
+        prueba = listaA[dato] in listaB
+    return prueba
+
+
+#EJERCICIO 5
+def hayDuplicados(lista):
+    for dato1 in lista:
+        contador = 0
+        for dato in lista:
+            if dato == dato1:
+                 contador += 1
+            if contador == 2:
+                return True
+    return False
+
+
+#EJERCICIO 6
+def borrarDuplicados(lista):
+    for dato1 in lista:
+        contador = 0
+        for dato in lista:
+            if dato == dato1:
+                 contador += 1
+            if contador == 2:
+                lista.remove(dato)
+    return lista
 
 
 
 
 
 def main():
+
     listaMagna = [[x for x in range(1,11)], [1,2], [5], [], [1,2,3],"ERROR", [1,9,9,6] ]
     print("Ejercicio 1:")
     for dato in range(0,4):
-        print ("Para la lista %s regresa el acumulado %s " %(listaMagna[dato],  sumarAcomulado(listaMagna[dato])))
+        print ("Para la lista %s regresa el acumulado %s " %(listaMagna[dato],  sumarAcumulado(listaMagna[dato])))
 
     print("\nEjercicio 2:")
-    for dato in range(0,len(listaMagna)):
+    for dato in range(0,5):
         if dato != 5:
             print ("Para la lista %s regresa %s " %(listaMagna[dato],  recortarLista(listaMagna[dato])))
         else:
@@ -66,6 +96,18 @@ def main():
     for dato in range(0, len(listaMagna),3):
         print("Para la lista %s regresa %s " % (listaMagna[dato], estanOrdenados(listaMagna[dato])))
 
+    print("\nEjercicio 4:")
+    print("FRENANDO es Anagrama de FERNANDO?: ", sonAnagramas("fernando","frenando"))
+    print("SILVA es Anagrama de SILVIA?: ", sonAnagramas("SILVA", "silvia"))
+    print("SI es Anagrama de SOL?: ", sonAnagramas("SI", "sOl"))
+
+    print("\nEjercicio 5:")
+    for dato in range(0, len(listaMagna), 3):
+        print("Para la lista %s regresa %s " % (listaMagna[dato], hayDuplicados(listaMagna[dato])))
+
+    print("\nEjercicio 6:")
+    print("Para la lista %s reresa %s " % (listaMagna[0], borrarDuplicados(listaMagna[0])))
+    print("Para la lista %s reresa %s " % ([1,9,9,6], borrarDuplicados([1,9,9,6])))
 
 main()
 
