8ac9e01711f369feab77dae65ec33365d2ef3503
Jossian <A01746745@itesm.mx>
2018-04-05 22:08:21 -0500

    terminada

---
+++ Tarea8.py
@@ -0,0 +1,59 @@
+#Jossian Abimelec García Quijano
+#efectua diferentes funciones de una lista
+def sumarAcumulado(lista):
+    nuevaLista=[]
+    i=0
+    for dato in lista:
+        i += dato
+        nuevaLista.append(i)
+    return nuevaLista
+
+    return nuevaLista
+
+
+def recortarLista(lista):
+    nuevaLista=[]
+    for k in range(1,len(lista)-1):
+
+        nuevaLista.append(lista[k])
+    return nuevaLista
+
+
+
+
+def estanOrdenador(lista):
+    for k in range(0, len(lista) - 1):
+        if lista[k] > lista[k + 1]:
+            return False
+    return True
+
+
+def hayDuplicados(lista):
+    for dato in lista:
+        vecesDat = lista.count(dato)
+        if vecesDat > 1:
+            return True
+        else:
+            return False
+
+
+
+
+def borrarDuplicados(lista):
+    for dato in lista:
+        veces = lista.count(dato)
+        if veces > 1:
+            lista.remove(dato)
+            return lista
+        else:
+            return lista
+
+
+def main():
+    lista=[1,2,3,4,5]
+    print("La lista",lista," regresa la lista acumulada ",sumarAcumulado(lista))
+    print("La lista original", lista," regresa", recortarLista(lista))
+    print(estanOrdenador(lista))
+    print(hayDuplicados(lista))
+    print("La lista ",lista,"regresa ",borrarDuplicados(lista))
+main()
